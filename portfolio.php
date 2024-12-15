9b6332d0af91c4d1bd0af0f4a4c3e787f9f2c84b
JustAdam <134600869+adam000000001@users.noreply.github.com>
2024-12-15 16:41:49 +0300

    Add files via upload

---
+++ portfolio.php
@@ -0,0 +1,11 @@
+<?php
+
+session_start();
+
+require_once __DIR__ . "/includes/config.php";
+require_once ROOT . "/includes/function.php";
+require_once ROOT . "/includes/db.php";
+
+$title = "Главная страница";
+
+require_once VIEWS . "/portfolio.tpl.php";
